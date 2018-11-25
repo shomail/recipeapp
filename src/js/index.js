1ec721877f15a6a095fba0b912a29b3668888b15
Shomail <shomail1988@gmail.com>
2018-11-25 23:03:37 +0100

    dev tools setup

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+// Global app controller
+import x from './test';
+const num = 44;
+console.log(`i imported ${x} from test module and used const ${num}`);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpacKPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpacKPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
