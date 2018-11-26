53aef17d32012e34590762239be8e3e041f9c98b
Shomail <shomail1988@gmail.com>
2018-11-26 09:16:14 +0100

    project structure and api connection established

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
-// Global app controller
-import x from './test';
-const num = 44;
-console.log(`i imported ${x} from test module and used const ${num}`);
+import axios from 'axios';
+
+async function getResults(query) {
+  const key = '813e28f5069bc003206ad25b59237660';
+  try {
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+getResults('pizza');
