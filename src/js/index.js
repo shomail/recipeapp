15cada6d163616c1339f7ea4e0d781c76b1f638b
Shomail <shomail1988@gmail.com>
2018-11-26 10:33:41 +0100

    rendering results view

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,30 @@
+import { elements } from './views/base';
 import Search from './models/Search';
+import * as searchView from './views/searchView';
 
 //Search obj, current recipe obj, shopping list obj, liked obj
 const state = {}
 
 const controlSearch = async () => {
   // get query from view
-  const query = 'pizza';
+  const query = searchView.getInput();
   
   if(query) {
     //new search obj and add to state
     state.search = new Search(query);
 
     //prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
     //search for recipes
     await state.search.getResults();
 
     //render results on UI
-    console.log(state.search.results);
+    searchView.renderResults(state.search.results);
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
