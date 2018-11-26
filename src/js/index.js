98f62cf631fef9d6d1c8beafd9dd06adf1dfea91
Shomail <shomail1988@gmail.com>
2018-11-26 09:25:43 +0100

    added Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const key = '813e28f5069bc003206ad25b59237660';
-  try {
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch(e) {
-    console.log(e)
-  }
-}
+const search = new Search('pizza');
 
-getResults('pizza');
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    const key = '813e28f5069bc003206ad25b59237660';
+    try {
+      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.recipes = res.data.recipes;
+      // console.log(this.recipes);
+    } catch(e) {
+      console.log(e)
+    }
+  }
+}
